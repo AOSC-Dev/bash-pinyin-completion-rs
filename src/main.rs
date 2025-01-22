d20392caa21812c3e63412a881fc827eddf24315
wxiwnd <wxiwnd@outlook.com>
2025-01-22 10:43:54 +0800

    init: first version
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use ib_pinyin::{matcher::PinyinMatcher, pinyin::PinyinNotation};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    // Print usage
+    if args.len() < 2 {
+        eprintln!("Usage: {} <pinyin>", args[0]);
+        std::process::exit(1);
+    }
+
+    let input: &str = &args[1];
+    let matcher = PinyinMatcher::builder(input)
+    .pinyin_notations(PinyinNotation::Ascii | PinyinNotation::AsciiFirstLetter)
+    .build();
+
+    let mut results: Vec<String> = Vec::new();
+    // Read current location
+    if let Ok(entries) = std::fs::read_dir(".") {
+        for entry in entries.flatten() {
+                let path = entry.path();
+                if let Some(file_name) = path.file_name() {
+                    if let Some(file_name_str) = file_name.to_str() {
+                        if matcher.is_match(file_name_str) {
+                            results.push(file_name_str.to_string());
+                        }
+                    }
+                }
+            }
+        }
+        for result in results {
+            println!("{}", result);
+        }
+    }
