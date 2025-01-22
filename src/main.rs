ca78249772033295d1bfadedc497fa4d73497b66
wxiwnd <wxiwnd@outlook.com>
2025-01-22 13:13:41 +0800

    enh: ignore filename without a single han
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ fn main() {
     .pinyin_notations(PinyinNotation::Ascii | PinyinNotation::AsciiFirstLetter)
     .build();
 
+    let han_re = regex::Regex::new(r"\p{Han}").unwrap();
+
     let mut results: Vec<String> = Vec::new();
     // Read current location
     if let Ok(entries) = std::fs::read_dir(".") {
@@ -20,7 +22,7 @@ fn main() {
                 let path = entry.path();
                 if let Some(file_name) = path.file_name() {
                     if let Some(file_name_str) = file_name.to_str() {
-                        if matcher.is_match(file_name_str) {
+                        if matcher.is_match(file_name_str) && han_re.is_match(file_name_str) {
                             results.push(file_name_str.to_string());
                         }
                     }
