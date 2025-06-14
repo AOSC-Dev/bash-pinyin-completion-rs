173eec9984abecd0ba97256e0223ccb4983d9b9a
Chen Linxuan <>
2025-06-14 21:11:07 +0800

    feat: support Shuangpin
    Signed-off-by: Chen Linxuan <me@black-desk.cn>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
 use ib_pinyin::{matcher::PinyinMatcher, pinyin::PinyinNotation};
+use std::env;
 use std::io::{BufRead, BufReader};
+
+fn parse_pinyin_notation_env() -> PinyinNotation {
+    let env_val = env::var("PINYIN_COMP_MODE").unwrap_or_default();
+    let mut notation = PinyinNotation::empty();
+    let mut shuangpin = Option::<PinyinNotation>::None;
+
+    for mode in env_val.split(',') {
+        let mode = mode.trim();
+        match mode {
+            "Quanpin" => {
+                notation |= PinyinNotation::Ascii;
+            }
+            "ShuangpinAbc" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterAbc);
+            }
+            "ShuangpinJiajia" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterJiajia);
+            }
+            "ShuangpinMicrosoft" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterMicrosoft);
+            }
+            "ShuangpinThunisoft" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterThunisoft);
+            }
+            "ShuangpinXiaohe" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterXiaohe);
+            }
+            "ShuangpinZrm" => {
+                shuangpin.get_or_insert(PinyinNotation::DiletterZrm);
+            }
+            _ => {}
+        }
+    }
+
+    notation |= shuangpin.unwrap_or(PinyinNotation::empty());
+
+    if notation.is_empty() {
+        notation = PinyinNotation::Ascii;
+    }
+
+    if notation == PinyinNotation::Ascii {
+        notation |= PinyinNotation::AsciiFirstLetter;
+    }
+
+    notation
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -12,8 +59,9 @@ fn main() {
 
     let input: &str = &args[1];
     if han_re.is_match(input) {return};
+    let notation = parse_pinyin_notation_env();
     let matcher = PinyinMatcher::builder(input)
-        .pinyin_notations(PinyinNotation::Ascii | PinyinNotation::AsciiFirstLetter)
+        .pinyin_notations(notation)
         .build();
 
     let stdin = std::io::stdin();
